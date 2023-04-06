632f1cc22f5bc82956d9fc899f9a35d503932149
Shank0071 <sankarpv.pmc@gmail.com>
2023-04-06 10:25:56 +0530

    latitude longitude added in popup

--- LocationDisplayInMaps/index.js
+++ LocationDisplayInMaps/index.js
@@ -1,14 +1,12 @@
 // Create a Leaflet map object
 const map = L.map("mapid");
 
-
 // Add a tile layer to the map
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
   maxZoom: 19,
 }).addTo(map);
-
 
 const ref = database.ref("users");
 ref.on("value", function (snapshot) {
@@ -22,11 +20,15 @@ ref.on("value", function (snapshot) {
     L.marker([item.lat, item.lon]).addTo(map);
     results7.push({
       id: item.id,
+      lat: item.lat,
+      lon: item.lon,
       markers: L.marker([item.lat, item.lon]).addTo(map),
     });
   });
   console.log(results7);
   results7.forEach((marker, index) => {
-    marker.markers.bindPopup(`My id is ${marker.id} <br> this is a`);
+    marker.markers.bindPopup(
+      `My id is ${marker.id} <br> latitude: ${marker.lat} <br> longitude: ${marker.lon}`
+    );
   });
 });
